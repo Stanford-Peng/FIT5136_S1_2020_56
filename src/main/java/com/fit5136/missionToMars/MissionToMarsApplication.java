e151df646a51c2aa2c6479a36af18c11ed178f6f
wenhe <wzha0102@student.monash.edu>
2020-05-12 22:33:09 +1000

    updatelogin

--- src/main/java/com/fit5136/missionToMars/MissionToMarsApplication.java
+++ src/main/java/com/fit5136/missionToMars/MissionToMarsApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-@EnableAutoConfiguration(exclude = {
+
+
+@SpringBootApplication(exclude = {
 		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
 })
 public class MissionToMarsApplication {
